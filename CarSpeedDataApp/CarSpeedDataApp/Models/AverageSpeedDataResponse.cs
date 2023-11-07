a52a0b9061675819b88b88eb64c7c6dcb930e110
= <judite.senkane.2018@gmail.com>
2023-11-07 15:01:33 +0200

    json property name re-added

--- CarSpeedDataApp/CarSpeedDataApp/Models/AverageSpeedDataResponse.cs
+++ CarSpeedDataApp/CarSpeedDataApp/Models/AverageSpeedDataResponse.cs
@@ -1,7 +1,10 @@
-﻿namespace CarSpeedDataApp.Models
+﻿using System.Text.Json.Serialization;
+
+namespace CarSpeedDataApp.Models
 {
     public class AverageSpeedDataResponse
     {
+        [JsonPropertyName("speed")]
         public List<double> AverageSpeedData { get; set; }
     }
 }
